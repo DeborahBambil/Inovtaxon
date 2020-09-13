f761a781389cf74b57e96a4641857056139df0bc
DeborahBambil <50251383+DeborahBambil@users.noreply.github.com>
2020-09-12 23:48:35 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,24 @@
+sudo apt install python-minimal
+sudo apt-get install python-opencv
+apt install python-pip
+sudo pip install -U scikit-image
+sudo apt-get install tk tk-dev
+sudo apt-get install build-essential python-dev
+sudo apt-get install python-pip python-numpy
+sudo apt-get install default-jdk
+pip install -U pip
+sudo python -m pip uninstall pip && sudo apt install python-pip --reinstall
+sudo apt install python-numpy or pip install numpy
+sudo apt-get install python-numpy python-scipy
+python -m pip install -U matplotlib
+python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+pip install ipython[all]
+pip install networkx
+sudo apt-get install libfreetype6
+pip install Cycler
+sudo apt-get install python-pil python-matplotlib python-pygraphviz
+sudo pip install javabridge==1.0.18
+sudo pip install python-weka-wrapper
+sudo apt-get install tk8.5
+sudo apt install python-pil.imagetk
+apt install linuxbrew-wrapper
